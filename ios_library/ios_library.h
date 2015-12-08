aef87255e24be70e1b458b1b0c17c9c2e8a23264
yarenty <yarenty@gmail.com>
2015-12-08 10:53:24 +0000

    Initial Commit

---
+++ ios_library/ios_library.h
@@ -0,0 +1,13 @@
+//
+//  ios_library.h
+//  ios_library
+//
+//  Created by Jaroslaw Nowosad on 08/12/2015.
+//  Copyright © 2015 SkyCorp Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ios_library : NSObject
+
+@end
---
+++ ios_library/ios_library.m
@@ -0,0 +1,13 @@
+//
+//  ios_library.m
+//  ios_library
+//
+//  Created by Jaroslaw Nowosad on 08/12/2015.
+//  Copyright © 2015 SkyCorp Ltd. All rights reserved.
+//
+
+#import "ios_library.h"
+
+@implementation ios_library
+
+@end
