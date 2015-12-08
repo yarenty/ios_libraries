f17c9937bcd3237db1be265f2194f73be42d24e5
yarenty <yarenty@gmail.com>
2015-12-08 11:16:49 +0000

    Initial Commit

---
+++ ios_library/ios_library.h
@@ -0,0 +1,19 @@
+//
+//  ios_library.h
+//  ios_library
+//
+//  Created by Jaroslaw Nowosad on 08/12/2015.
+//  Copyright © 2015 SkyCorp Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios_library.
+FOUNDATION_EXPORT double ios_libraryVersionNumber;
+
+//! Project version string for ios_library.
+FOUNDATION_EXPORT const unsigned char ios_libraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_library/PublicHeader.h>
+
+
